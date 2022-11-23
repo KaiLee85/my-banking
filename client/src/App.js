7842886a5bfba4457d7004a631d704397135e8e0
KaiLee85 <87049080+KaiLee85@users.noreply.github.com>
2022-11-22 20:50:42 -0700

    server connect

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [form, setForm] = useState({
+    amount: 0,
+    description: "",
+    date: "",
+  });
+
+  function handleInput(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const res = await fetch("http://localhost:4000/transaction", {
+      method: "POST",
+      body: form,
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          name="amount"
+          value={form.amount}
+          onChange={handleInput}
+          placeholder="Enter transaction amount"
+        ></input>
+
+        <input
+          type="text"
+          name="description"
+          value={form.description}
+          onChange={handleInput}
+          placeholder="Enter transaction details"
+        ></input>
+
+        <input type="date" name="date" value={form.date} />
+        <button type="submit">SUBMIT</button>
+      </form>
+    </>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
 const PORT = 4000;
 const app = express();
+app.use(cors());
+mongoose
+  .connect(
+    "mongodb+srv://mybanking:mybanking@mybanking.aoggz8g.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("mongooDB is connected"))
+  .catch((err) => console.log(err));
+
+// await mongoose.connect(
+//   "mongodb+srv://mybanking:mybanking@mybanking.aoggz8g.mongodb.net/?retryWrites=true&w=majority"
+// );
+// console.log("mongooDB is connected");
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.listen(PORT, () => {
+  console.log("server is running");
 });
